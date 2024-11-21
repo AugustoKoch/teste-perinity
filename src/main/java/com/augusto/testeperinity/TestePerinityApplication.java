eb729b95b6fa88059a70f170a7266a3fe30cded9
Augusto Koch <augustok872@gmail.com>
2024-11-21 12:09:27 -0300

    Início do projeto

---
+++ src/main/java/com/augusto/testeperinity/TestePerinityApplication.java
@@ -0,0 +1,13 @@
+package com.augusto.testeperinity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestePerinityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestePerinityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/augusto/testeperinity/TestePerinityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augusto.testeperinity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestePerinityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
